f0bda55a96f96e03a1f7c2543366d95d805db7e0
Napoleon Patague <napoleon.patague@gmail.com>
2022-03-16 17:09:19 +0800

    change schedule view

--- src/layouts/sidebar/Sidebar.vue
+++ src/layouts/sidebar/Sidebar.vue
@@ -19,7 +19,7 @@
         </v-list-item-avatar>
 
         <v-list-item-content>
-          <v-list-item-title>{{ showItemState }}</v-list-item-title>
+          <v-list-item-title>Admin</v-list-item-title>
         </v-list-item-content>
       </v-list-item>
       <!---USer Area -->
